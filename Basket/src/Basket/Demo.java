433e91feed3eb0c036c4bd554d4e60a6cbdf2cce
“safaa <s.hriez@htu.edu.jo>
2024-01-06 22:29:38 +0300

    add object

--- Basket/src/Basket/Demo.java
+++ Basket/src/Basket/Demo.java
@@ -4,7 +4,9 @@ package Basket;
 
 public class Demo {
     public static void main(String[] args) {
-        
+      Basket <Double, Double> b = new Basket<>();
+      b.add(2.4);
+      b.add(5.6);
       
     }
 }
