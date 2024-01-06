c3cf21fa7285fbf94cfe1ff693b011e1a26713c1
“safaa <s.hriez@htu.edu.jo>
2024-01-06 22:50:05 +0300

    added both 3

--- Basket/src/Basket/Demo.java
+++ Basket/src/Basket/Demo.java
@@ -5,8 +5,9 @@ package Basket;
 public class Demo {
     public static void main(String[] args) {
       Basket <Double, Double> b = new Basket<>();
-      b.add(2.4);
+      b.add(3.0);
       b.add(5.6);
+      b.add(3.0);
       b.add(7.0);
       System.out.println(b);
         System.out.println("safaa added");
